d29dd88b1a8c595b604f3bc59e756bf6b11e35fd
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2018-11-02 11:25:12 -0500

    simplify

--- src/fetchApi.js
+++ src/fetchApi.js
@@ -1,7 +1,6 @@
 const API_URL = "https://api.chucknorris.io/jokes/"
 
-export default (endpoint = "random") => {
-  return fetch(`${API_URL}${endpoint}`, { mode: "cors" })
+export default (endpoint = "random") =>
+  fetch(`${API_URL}${endpoint}`, { mode: "cors" })
     .then(response => response.json())
     .then(data => data)
-}
